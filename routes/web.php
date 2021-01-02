e7386b2cb0d2eaf686c15992bdadb414690743ab
jenishk20 <kotharijenish2001@gmail.com>
2021-01-02 11:43:21 +0530

    Login and Signup😂

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',function(){
+
+
+    return view('home');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
