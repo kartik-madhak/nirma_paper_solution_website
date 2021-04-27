8c7a87d0d5c7e0771e70a389b3f4673b273f7064
jenishk20 <kotharijenish2001@gmail.com>
2021-04-27 21:08:05 +0530

    Email Configuration Changes 😁😁

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
             $cc[$user->name]=count($user->answers);
         }
 
-        $chart_options = [
+        $chart_options1 = [
             'chart_title' => 'Users by months',
             'report_type' => 'group_by_date',
             'model' => 'App\Models\User',
@@ -27,12 +27,28 @@ class AdminController extends Controller
             'chart_type' => 'line',
         ];
         try {
-            $chart1 = new LaravelChart($chart_options);
+            $chart1 = new LaravelChart($chart_options1);
         } catch (\Exception $e) {
             dd($e);
         }
 
+        $chart_options2 = [
+            'chart_title' => 'Users by months',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\User',
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'day',
+            'chart_type' => 'bar',
+        ];
+        try {
+            $chart2 = new LaravelChart($chart_options2);
+        } catch (\Exception $e) {
+            dd($e);
+        }
+
+
+
 //        dd($chart1->renderHtml());
-        return view('admin.index',compact('users','chart1','cc'));
+        return view('admin.index',compact('users','chart1','chart2','cc'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,57 +1,79 @@
-@extends('layouts.app')
+{{--@extends('layouts.app')--}}
 {{--@section('head')--}}
 
 {{--     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>--}}
 
 
+{{--@endsection--}}
+{{--@section('content')--}}
+
+{{--    <div style="background-color: white">--}}
+{{--        <div class="container">--}}
+{{--            <div class="row justify-content-center">--}}
+{{--                <div class="col-md-8 d-inline-flex">--}}
+{{--                    <div class="card   cool-card">--}}
+{{--                        <div class="card-header">Dashboard</div>--}}
+
+{{--                        <div class="card-body ">--}}
+{{--                            --}}{{--                        {{dd($chart1)}}--}}
+{{--                            <h1>{{ $chart1->options['chart_title'] }}</h1>--}}
+{{--                            {!! $chart1->renderHtml() !!}--}}
+
+{{--                        </div>--}}
+
+{{--                    </div>--}}
+{{--                    <div class="card">--}}
+{{--                        <div class="card-header">Dashboard</div>--}}
+
+{{--                        <div class="card-body">--}}
+
+{{--                            <h1>{{ $chart1->options['chart_title'] }}</h1>--}}
+{{--                            {!! $chart1->renderHtml() !!}--}}
+
+{{--                        </div>--}}
+
+{{--                    </div>--}}
+
+
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+{{--    </div>--}}
+{{--   --}}
+{{--    {!! $chart1->renderChartJsLibrary() !!}--}}
+{{--    {!! $chart1->renderJs() !!}--}}
+
 
 {{--@endsection--}}
+
+@extends('layouts.app')
+
 @section('content')
-
-
-
-    <div class="container text-light">
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8 d-inline-flex">
-                <div class="card bg-white">
+            <div class="col-md-8">
+                <div class="card">
                     <div class="card-header">Dashboard</div>
 
-                    <div class="card-body text-light" >
-{{--                        {{dd($chart1)}}--}}
-                        <h1 class="text-light">{{ $chart1->options['chart_title'] }}</h1>
-                        {!! $chart1->renderHtml() !!}
-
-                    </div>
-
-                </div>
-                <div class="card bg-white">
-                    <div class="card-header">Dashboard</div>
-
-                    <div class="card-body text-light" >
+                    <div class="card-body">
 
                         <h1>{{ $chart1->options['chart_title'] }}</h1>
                         {!! $chart1->renderHtml() !!}
 
+                        <hr />
+
+
+
+
                     </div>
 
                 </div>
-
-
-
             </div>
         </div>
     </div>
-
-
-    <div class="container-fluid text-light">
-        @foreach($users as $user)
-            {{$user->name}}
-            {{$cc[$user->name]}}
-        @endforeach
-    </div>
     {!! $chart1->renderChartJsLibrary() !!}
+    {!! $chart2->renderChartJsLibrary() !!}
     {!! $chart1->renderJs() !!}
-
-
+    {!! $chart2->renderJs() !!}
 @endsection
-
